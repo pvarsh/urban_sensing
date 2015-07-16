6fcc9b4bb6063220f52ceefac7923987454be981
Peter Varshavsky <pvarsh@gmail.com>
2015-07-15 21:59:54 -0400

    Added waiting for button press to take picture

--- button.py
+++ button.py
@@ -1,18 +1,22 @@
 import time
 
+import picamera
 import RPi.GPIO as GPIO
 
 
 # set some constants
 LED_PIN = 4
 BUTTON_PIN = 18
+NUM_IMAGES = 6
+FRAME_RATE = 2
+RESOLUTION = (1024, 768)
 
 def cleanup_and_raise(exc):
     print("\nCleaning up GPIO")
     GPIO.cleanup()
     raise exc
 
-def main():
+def main_():
     GPIO.setmode(GPIO.BCM)
     
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -30,6 +34,35 @@ def main():
             prev_input_state = input_state
         time.sleep(0.05)
 
+def setup_GPIO():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(LED_PIN, GPIO.OUT)
+
+def main():
+    
+    setup_GPIO()
+   
+    while True:
+        
+        GPIO.wait_for_edge(BUTTON_PIN, GPIO.BOTH)
+        now = time.time()
+        now_str = datetime.datetime.fromtimestamp(now).strftime('%Y%m%d_%H%M%S')
+
+        img_filenames = (
+            'img_{}_{:02d}.png'.format(now_str, i) for i in range(NUM_IMAGES)
+            )            
+
+        img_filepaths = (
+            os.path.join('/home/pi/projects/rats', fn) for fn in img_filenames
+            )
+
+        with picamera.PiCamera() as camera:
+            camera.resolution = RESOLUTION 
+            camera.framerate = FRAME_RATE
+            GPIO.output(LED_PIN, True)
+            camera.capture_sequence(img_filepaths)
+            GPIO.output(LED_PIN, False)
 
 if __name__ == "__main__":
     try:
@@ -38,4 +71,8 @@ if __name__ == "__main__":
         cleanup_and_raise(exc)
     except Exception, exc:
         cleanup_and_raise(exc)
+    
+    # clean up on normal exit
+    print("\nCleaning up GPIO")
+    GPIO.cleanup()
 
