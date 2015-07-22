8a9e906e830923facc41ab8c2b62bfe5e6893327
jsmedmar <juansantiago.medina@gmail.com>
2015-07-22 14:40:11 -0400

    Adds rat_detector()

--- juan_detector.py
+++ juan_detector.py
@@ -6,6 +6,9 @@ import numpy as np
 import argparse
 import cv2
 import scipy.ndimage as nd
+import os
+import glob
+import time
 
 # initialize the current frame of the video, along with the list of
 # ROI points along with whether or not this is input mode
@@ -17,7 +20,7 @@ inputMode = False
 def selectROI(event, x, y, flags, param):
     # grab the reference to the current frame, list of ROI
     # points and whether or not it is ROI selection mode
-    global frame, roiPts, inputMode
+    global roiPts, inputMode
 
     # if we are in ROI selection mode, the mouse was clicked,
     # and we do not already have four points, then update the
@@ -34,145 +37,129 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-v", "--video",
                     help="path to the (optional) video file")
+    ap.add_argument("-im", "--images",
+                    help="path to the images folder")
     args = vars(ap.parse_args())
 
-    # grab the reference to the current frame, list of ROI
-    # points and whether or not it is ROI selection mode
-    global frame, roiPts, inputMode
-
-    # if the video path was not supplied, grab the reference to the
-    # camera
-    if not args.get("video", False):
+    # load argument and take action
+    if not args.get("video", False) and not args.get("images", False):
         camera = cv2.VideoCapture(0)
-
-    # otherwise, load the videov
+        _if_camera(camera)
+    elif not args.get("video", False) and args.get("images", True):
+        frames_path = os.path.join(args["images"], "*.png")
+        _if_images(frames_path)
     else:
         camera = cv2.VideoCapture(args["video"])
+        _if_camera(camera)
 
-    # setup the mouse callback
-    cv2.namedWindow("frame")
-    cv2.setMouseCallback("frame", selectROI)
 
-    # initialize the termination criteria for cam shift, indicating
-    # a maximum of ten iterations or movement by a least one pixel
-    # along with the bounding box of the ROI
-    termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-    roiBox = None
-
-    flag = True
-
-    # grab background
-    (grabbed, p_frame) = camera.read()
-    p_frame_mean = p_frame.mean(axis=2)
+def _if_images(frames_path):
+    """If ist images
+    """
+    # read images
+    frames_paths = [p for p in glob.glob(frames_path)]
+    frames = np.array([nd.imread(p) for p in frames_paths])
 
     # start dilatation, erosion and mass label
     bdilation = nd.morphology.binary_dilation
     berosion = nd.morphology.binary_erosion
     meas_label = nd.measurements.label
 
-    # keep looping over the frames
+    # for frame in frames:
+    #     frame = np.square(frame) * 255.
+    #     frame = frames / frame.max()
+
+    # compute background using mean of im
+    background = frames.mean(axis=3).mean(axis=0)
+
+    for frame in frames:
+        # cv2.imshow("frame", background)
+        stop = rat_detector(grabbed=True,
+                            frame=frame,
+                            background=background,
+                            bdilation=bdilation,
+                            berosion=berosion,
+                            meas_label=meas_label)
+
+        time.sleep(1. / 15)
+
+
+def _if_camera(camera):
+    # start dilatation, erosion and mass label
+    bdilation = nd.morphology.binary_dilation
+    berosion = nd.morphology.binary_erosion
+    meas_label = nd.measurements.label
+
+    # grab background
+    (grabbed, background) = camera.read()
+    background = background.mean(axis=2)
+
     while True:
         # grab the current frame
         (grabbed, frame) = camera.read()
-        frame_mean = frame.mean(axis=2)
-
-        # compute difference
-        if frame_mean.mean() > p_frame_mean.mean():
-            diff = ((frame_mean - p_frame_mean) > 50) * 1.
-        else:
-            diff = ((-frame_mean + p_frame_mean) > 50) * 1.
-
-        # set size threshold
-        sz_thr = 3000
-        diff = bdilation(berosion(np.abs(diff)),iterations=2) * 1.
-        labs = meas_label(diff)
-
-        diff[:] = 0
-        rats = 0
-        for lab in range(1,labs[1]+1):
-            if 1.*(labs[0]==lab).sum() > sz_thr:
-                diff += (labs[0]==lab) * 1.
-                cnt = ((labs[0]==lab)*255).astype(np.uint8)
-                contours,hierarchy = cv2.findContours(cnt, 1, 2)
-                cnt = contours[-1]
-
-                # enclosing rectangle - not used
-                x,y,w,h = cv2.boundingRect(cnt)
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
-
-                # Enclosing circle
-                # (x,y),radius = cv2.minEnclosingCircle(cnt)
-                # center = (int(x),int(y))
-                # radius = int(radius)
-                # cv2.circle(frame,center,radius,(0,255,0),2)
-
-                rats += 1
-
-        # check to see if we have reached the end of the
-        # video
-        if not grabbed:
+        stop = rat_detector(grabbed=grabbed,
+                            frame=frame,
+                            background=background,
+                            bdilation=bdilation,
+                            berosion=berosion,
+                            meas_label=meas_label)
+        if stop:
             break
-
-        # if the see if the ROI has been computed
-        if roiBox is not None:
-            # convert the current frame to the HSV color space
-            # and perform mean shift
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            backProj = cv2.calcBackProject([hsv], [0], roiHist, [0, 180], 1)
-
-            # apply cam shift to the back projection, convert the
-            # points to a bounding box, and then draw them
-            (r, roiBox) = cv2.CamShift(backProj, roiBox, termination)
-            pts = np.int0(cv2.cv.BoxPoints(r))
-            cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
-
-        # show the frame and record if the user presses a key
-        print rats
-        cv2.imshow("frame", frame)
-        cv2.imshow("diff", diff)
-        key = cv2.waitKey(1) & 0xFF
-
-        # handle if the 'i' key is pressed, then go into ROI
-        # selection mode
-        if key == ord("i") and len(roiPts) < 4:
-            # indicate that we are in input mode and clone the
-            # frame
-            inputMode = True
-            orig = frame.copy()
-
-            # keep looping until 4 reference ROI points have
-            # been selected; press any key to exit ROI selction
-            # mode once 4 points have been selected
-            while len(roiPts) < 4:
-                cv2.imshow("frame", frame)
-                cv2.waitKey(0)
-
-            # determine the top-left and bottom-right points
-            roiPts = np.array(roiPts)
-            s = roiPts.sum(axis = 1)
-            tl = roiPts[np.argmin(s)]
-            br = roiPts[np.argmax(s)]
-
-            # grab the ROI for the bounding box and convert it
-            # to the HSV color space
-            roi = orig[tl[1]:br[1], tl[0]:br[0]]
-            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-            #roi = cv2.cvtColor(roi, cv2.COLOR_BGR2LAB)
-
-            # compute a HSV histogram for the ROI and store the
-            # bounding box
-            roiHist = cv2.calcHist([roi], [0], None, [16], [0, 180])
-            roiHist = cv2.normalize(roiHist, roiHist, 0, 255, cv2.NORM_MINMAX)
-            roiBox = (tl[0], tl[1], br[0], br[1])
-
-        # if the 'q' key is pressed, stop the loop
-        elif key == ord("q"):
-            break
-
-
-    # cleanup the camera and close any open windows
     camera.release()
     cv2.destroyAllWindows()
+
+
+def rat_detector(frame, background,
+                 bdilation, berosion, meas_label, grabbed=True):
+    """This function detects rat within img"""
+
+    th = 15
+    # setup the mouse callback
+    # cv2.namedWindow("frame")
+
+    # compute gray_scale
+    frame_mean = frame.mean(axis=2)
+
+    diff = ((frame_mean - background) > th) * 1.
+    # cv2.imshow("diff", diff)
+
+    # set size threshold
+    sz_thr = th + 5
+    diff = bdilation(berosion(np.abs(diff), iterations=2),
+                     iterations=20) * 1.
+    labs = meas_label(diff)
+    cv2.imshow("diff2", diff)
+
+    diff[:] = 0
+    rats = 0
+    for lab in range(1, labs[1] + 1):
+        if 1. * (labs[0] == lab).sum() > sz_thr:
+            rats += 1
+            diff += (labs[0] == lab) * 1.
+            cnt = ((labs[0] == lab) * 255).astype(np.uint8)
+            contours, hierarchy = cv2.findContours(cnt, 1, 2)
+            cnt = contours[-1]
+
+            # enclosing rectangle - not used
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(frame, (x, y), (x + w, y + h),
+                          (0, 255, 0), 1)
+
+    # check to see if we have reached the end of the video
+    if not grabbed:
+        return True
+
+    # show the frame and record if the user presses a key
+    cv2.putText(frame, "Rats in Scene: {}".format(rats), (10, 20),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+    cv2.imshow("frame", frame)
+    # cv2.imshow("diff", diff)
+
+    # if the 'q' key is pressed, stop the loop
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        return True
 
 if __name__ == "__main__":
     main()
